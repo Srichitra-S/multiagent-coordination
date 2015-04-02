95e186716ec466b5c878f702237064f3f51fe278
skchandra <shivali.chandra@students.olin.edu>
2015-04-02 12:14:48 -0400

    Modified to send motor commands with user input, got rid of time delay

--- send_motor_cmd.py
+++ send_motor_cmd.py
@@ -15,31 +15,10 @@ def send_udp(left_speed,right_speed,udp_port=7070,udp_ip="192.168.17.150"):
     sock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
     sock.sendto(message, (udp_ip,udp_port))
-    sock.recvfrom(0)
 
-while 1:
-    #send_udp(100,100)
-    #time.sleep(10)
-    talker()
-    if raw_input()=="q":
-        break
-
-
-"""def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
-
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass"""
-
-       
-    
+left = ''
+right = ''
+while (left!='-1') and (right!='-1'):
+    left = raw_input("left")
+    right = raw_input("right")
+    send_udp(left,right)
