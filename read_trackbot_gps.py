c14ec2e9458a8ca8010a3be2c6f8e9d0e0075619
skchandra <shivali.chandra@students.olin.edu>
2015-04-13 11:07:59 -0400

    Current code to read data from GPS on trackbot

--- read_trackbot_gps.py
+++ read_trackbot_gps.py
@@ -1,6 +1,15 @@
-import socket
+import socket 
+from sh import nc
 
-UDP_IP = "192.168.50.37"
+
+"""while True:
+	output = nc('-ulv','192.168.17.150','61557') 
+	print output"""
+
+
+UDP_IP = "192.168.16.57"
+BOT_IP = "192.168.17.150"
+#BOT_IP = "192.168.16.52"
 UDP_PORT = 61557
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -8,5 +17,5 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+	print "received message:", data
